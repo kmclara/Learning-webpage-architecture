c94b99e4a1976a39bab9940cc71b214246583e31
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-01-26 14:17:03 -0300

    feat: draft code

---
+++ Header/index.js
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <header>
+      <h1>Página do mateus</h1>
+      <a href="">menu 1</a>
+      <a href="">menu 2</a>
+      <a href="">menu 3</a>
+    </header>
+  )
+}
